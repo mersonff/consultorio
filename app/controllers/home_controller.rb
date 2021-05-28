df9df6c0b5e8297d871f0d85c47ecdf892af8e65
Emerson Freitas <mersonsi@gmail.con>
2021-05-28 13:26:41 -0300

    Modificaçoes na interface

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
 
-    def dashboard
-        agora = DateTime.current
-        @consultas_realizadas = Appointment.where('ends_at < ?', agora).paginate(page: params[:page], per_page: 5)
+    def index
+        
+        now = DateTime.current
+        @doctors = Doctor.paginate(page: params[:page], per_page: 5)
+
+        @consultas_realizadas = Appointment.where('ends_at < ?', now).paginate(page: params[:page], per_page: 5)
         @quantidade_de_consultas_ja_realizadas = @consultas_realizadas.count
-        @pacientes_atendidos_hoje = Patient.joins(:appointments).where('ends_at = ?', agora).count
+        @pacientes_atendidos_hoje = Patient.joins(:appointments).where('ends_at = ?', now).count
+
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root "home#dashboard"
+  root "home#index"
 
   resources :patients, except: [:show]
   resources :doctors, expect: [:show] 
