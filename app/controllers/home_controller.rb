754fb50a8f4837429301131c6833686af818e779
Emerson Freitas <mersonsi@gmail.con>
2021-05-28 11:19:43 -0300

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
         agora = DateTime.current
         @consultas_realizadas = Appointment.where('ends_at < ?', agora).paginate(page: params[:page], per_page: 5)
         @quantidade_de_consultas_ja_realizadas = @consultas_realizadas.count
-        @pacientes_atendidos_hoje = Patient.joins(:appointments).where('date(ends_at) = ?', agora.strftime('%d/%m/%Y')).count
+        @pacientes_atendidos_hoje = Patient.joins(:appointments).where('ends_at = ?', agora).count
     end
 end
